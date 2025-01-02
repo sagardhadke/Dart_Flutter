554655cb069babfe653c720c86aa2e54c6182eff
Sagar Dhadke <peaceunique2003@gmail.com>
2025-01-02 23:29:33 +0530

    All DataTypes with print

--- datatypes_variables.dart
+++ datatypes_variables.dart
@@ -10,11 +10,21 @@ void main() {
 
   List<String> cars = ['AUdi', 'BMW', 'Toyota'];
   Map<String, dynamic> myData = {
-    'api_key' : 'abc123abckey',
-    'age' : 20,
-    'name' : 'sagar dhadke',
-    'role' : 'junior flutter developer' 
+    'api_key': 'abc123abckey',
+    'age': 20,
+    'name': 'sagar dhadke',
+    'role': 'junior flutter developer'
   };
+
+  print("Name $name");
+  print("Age $age");
+  print("Price $price");
+  print("isActive $isActive");
+  print("Amount $amount");
+  print("Key $key");
+
+  print("List $cars");
+  print("Map $myData");
 }
 
 // single line comment
