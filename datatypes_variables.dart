ea2fdffd17583ed65853e726289e8e2221bcb429
Sagar Dhadke <peaceunique2003@gmail.com>
2025-01-02 23:26:34 +0530

    All DataTypes

--- datatypes_variables.dart
+++ datatypes_variables.dart
@@ -5,5 +5,20 @@ void main() {
   int age = 20;
   double price = 12.20;
   bool isActive = true;
-  
+  var amount = 120000;
+  dynamic key = 'API-KEY';
+
+  List<String> cars = ['AUdi', 'BMW', 'Toyota'];
+  Map<String, dynamic> myData = {
+    'api_key' : 'abc123abckey',
+    'age' : 20,
+    'name' : 'sagar dhadke',
+    'role' : 'junior flutter developer' 
+  };
 }
+
+// single line comment
+
+/*
+Multi
+Line Comment */
